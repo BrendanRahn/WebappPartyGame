066ed09075470daaf8b8248ccc7045d75be30a59
Brendan <brendanrahn01@gmail.com>
2024-01-16 23:06:52 -0600

    starting to implement div for inputing new song

--- static/main_page_script.js
+++ static/main_page_script.js
@@ -15,7 +15,7 @@ function animate_spinner() {
 
     var current_animations = element.getAnimations()
 
-    //if multiple animations are applied to this element this will not work correctly
+    //if multiple animations are applied to this element this if statement will not work correctly
     if (current_animations.length == 0) {
         const anim = element.animate({transform: "rotate(360deg)"}, 2000);
         anim.id = "spin_animation"
