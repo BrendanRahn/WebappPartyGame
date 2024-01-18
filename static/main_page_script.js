bf3ce0ca9902c64de96e6856aad8bf5d0cfa172d
Brendan <brendanrahn01@gmail.com>
2024-01-18 13:28:07 -0600

    added css to input text div

--- static/main_page_script.js
+++ static/main_page_script.js
@@ -11,16 +11,34 @@ myelem.addEventListener("click", function (event) {
 }
 
 function animate_spinner() {
-    var element = document.getElementById("needle");
+    var spinner = document.getElementById("needle");
 
-    var current_animations = element.getAnimations()
+    var current_animations = spinner.getAnimations()
 
     //if multiple animations are applied to this element this if statement will not work correctly
     if (current_animations.length == 0) {
-        const anim = element.animate({transform: "rotate(360deg)"}, 2000);
-        anim.id = "spin_animation"
+        const animation = spinner.animate({transform: "rotate(360deg)"}, 2000);
+        animation.id = "spin_animation";
     }
     
 
     
+}
+
+function show_input_div() {
+    var input_div = document.getElementById("input_div")
+    
+    input_div.style.display = "block"
+    input_div.style.opacity = 0
+    
+    const animation = input_div.animate([
+        {opacity: 0, offset: 0},
+        {transform: "scale(150%)", offset:0.5},
+        {opacity: 1, offset: 1},
+        {transform: "scale(100%)", offset: 1}
+    ], 500);
+    
+    input_div.style.opacity = 1
+
+    console.log("hit")
 }
